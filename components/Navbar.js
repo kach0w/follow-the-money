b5c2c3d8bc39f43e5c3010deb3e050662ad13428
Kachow <karsab343@gmail.com>
2023-07-31 21:37:51 -0700

    'yooo'

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,11 +4,11 @@ import Link from "next/link";
 export const Navbar = () => {
   return (
     <div className='relative w-[100vw] align-middle'>
-        <a href="/">
+        <Link href="/">
             <div className='absolute inset-y-0 left-0 w-[35rem]'>
                 <h2 className='inline'>Follow the Money</h2>
             </div>
-        </a>
+        </Link>
         <div className="absolute inset-y-0 right-0 pr-5 w-[35rem] space-x-8 pt-2 ">
             <Link className="" href="/issues">Issues</Link>
             <Link className="" href="/search">Search</Link>
